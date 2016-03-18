2ee03e0bc3d2566b8eff726856f37c31b0f41d6f
P3nG <andreasstaps@gmail.com>
2016-03-18 22:39:55 +0100

    First Project commit
    Trying to commit the project, setting some folders to ignore

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Go down from square route of 600851475143 =>			775,146	(in -2 steps)
+            If n is prime and X % prime == 0 => n is biggest prime factor			
+            */
+
+            var bigNumber = 600851475143;
+            var bigNumberSquare = (int)Math.Sqrt(bigNumber);
+
+            if (bigNumberSquare % 2 == 0)
+                bigNumberSquare--;
+
+            var wantedPrime = 0;
+
+            for(var counter = bigNumberSquare; counter > 2; counter -= 2)
+            {
+                if (!IsPrime(counter))
+                    continue;
+
+                if (bigNumber % counter == 0)
+                {
+                    wantedPrime = counter;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Wanted prime factor: " + wantedPrime);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Returns if the given number is a prime.
+        /// Stolen from http://www.dotnetperls.com/prime.
+        /// </summary>
+        /// <param name="candidate">prime candidate</param>
+        /// <returns>true if candidate is a prime, false else</returns>
+        public static bool IsPrime(int candidate)
+        {
+            // Test whether the parameter is a prime number.
+            if ((candidate & 1) == 0)
+                return candidate == 2;
+
+            // Note:
+            // ... This version was changed to test the square.
+            // ... Original version tested against the square root.
+            // ... Also we exclude 1 at the end.
+            for (int i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                    return false;
+            }
+
+            return candidate != 1;
+        }
+    }
+}
---
+++ ProjectEuler/ProjectEuler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("ProjectEuler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectEuler")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("450a2ef3-6e1e-40f9-9097-7eca48bc4d3b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
